ee411a59443c8d20b56eb9e6db8ea4bd31b60c7c
Ganesh Sittampalam <ganesh@earth.li>
2021-11-26 19:11:12 +0000

    add `onRequest` for prompt example

--- server.ts
+++ server.ts
@@ -11,6 +11,12 @@ connection.onNotification(notification, (param: string) => {
 	console.log(param); // This prints Hello World
 });
 
+const getPromptReq = new rpc.RequestType<{}, {x: number}, void>('getPrompt');
+connection.onRequest(getPromptReq, (params: {}) => {
+  const {} = params;
+  return { x: 5 };
+});
+
 connection.listen();
 // process.stdin.on('data', (data) => console.log(data.toString()));
 console.log("listening");
